9e122628fa642d1ce9b5d51805ec1faa14ff5b68
Keith Hubert <keith.hubert@gmail.com>
2017-12-20 14:38:55 -0500

    redux schema mockup

--- firebase-schema.js
+++ firebase-schema.js
@@ -1,118 +1,116 @@
-// No Perfect
-//     -Tradeoffs
-// Keep Shallow
-// Repeat Data is OK
+No Perfect
+    -Tradeoffs
+Keep Shallow
+Repeat Data is OK
 
-// Here are the views we need:
+Here are the views we need:
 
-// Home
-//     All Ducks
+Home
+    All Ducks
 
-// Profile
-//     User Info
-//     Users Ducks
+Profile
+    User Info
+    Users Ducks
 
-// Replies 
-//     Specific Duck
-//     Ducks Replies
+Replies 
+    Specific Duck
+    Ducks Replies
 
-// Mapping out schemas: the following don't work
+Mapping out schemas: the following don't work:
 
-// /users
-//     uid
-//         ducks
-//             duckID
-//                 replies
-//                     replyID
-//                         uid
-//                         name
-//                         comment
-//                         etc
-//                 info
-//                     uid
-//                     avatar
-//                     text
-//                     numberOfLikes
-//                     etc
-//         likes
-//             duckID
+/users
+    uid
+        ducks
+            duckID
+                replies
+                    replyID
+                        uid
+                        name
+                        comment
+                        etc
+                info
+                    uid
+                    avatar
+                    text
+                    numberOfLikes
+                    etc
+        likes
+            duckID
 
-// _____________________________________
+_____________________________________
 
-// /ducks
-//     duckID 
-//         replies
-//             replyID
-//                 name
-//                 comment
-//                 etc
-//         info
-//             avatar
-//             text
-//             etc
-//         likes
-//             uid
+/ducks
+    duckID 
+        replies
+            replyID
+                name
+                comment
+                etc
+        info
+            avatar
+            text
+            etc
+        likes
+            uid
 
-// Instead repeat data, it's ok!
+Instead repeat data, it's ok!
 
-// /users
-//     uid
-//         info
-//     ducks
-//         duckID
-//             replies
-//             info
-//             usersWhoHaveLiked
-//     likes
-//         duckID
+/users
+    uid
+        info
+    ducks
+        duckID
+            replies
+            info
+            usersWhoHaveLiked
+    likes
+        duckID
     
-// /ducks
-//     duckID 
-//         replies
-//         info  (instead of listing out name, uid, avatar etc)
+/ducks
+    duckID 
+        replies
+        info  (instead of listing out name, uid, avatar etc)
             
-// Almost there! Not shallow enough, final schema:
+Almost there! Not shallow enough, final schema:
 
-// _____________________________________
+_____________________________________
 
-// /users
-//     uid
-//         name
-//         uid
-//         avatar
+/users
+    uid
+        name
+        uid
+        avatar
 
-// /ducks
-//     duckID
-//         avatar
-//         duckID
-//         name
-//         text
-//         timestamp
-//         uid
+/ducks
+    duckID
+        avatar
+        duckID
+        name
+        text
+        timestamp
+        uid
 
-// /likeCount ( could be in usersDucks, or ducks, but then would have to maintain synched state in both)
-//     duckID
+/likeCount ( could be in usersDucks, or ducks, but then would have to maintain synched state in both)
+    duckID
 
-// /usersDucks
-//     uid
-//         avatar
-//         duckID
-//         name
-//         text
-//         timestamp
-//         uid
+/usersDucks
+    uid
+        avatar
+        duckID
+        name
+        text
+        timestamp
+        uid
 
-// /replies
-//     duckID 
-//         replyID
-//             name
-//             comment
-//             uid
-//             timestamp
-//             avatar
+/replies
+    duckID 
+        replyID
+            name
+            comment
+            uid
+            timestamp
+            avatar
 
-// /usersLikes
-//     uid
-//         duckID
-
-// _____________________________________
+/usersLikes
+    uid
+        duckID
---
+++ redux-schema.js
@@ -0,0 +1,129 @@
+What's the minimal representation of our app state as an object? 
+
+
+Brought in established schema from firebase-schema:
+"Hello World version, misses important UI elements"
+
+// {
+//     users: {
+//         [uid]: {
+//             info: {
+//                 name,
+//                 uid,
+//                 avatar,
+//             }
+//         }
+//     }
+//     ducks: {
+//         [duckID]: {
+//             info: {
+//                 avatar
+//                 duckID
+//                 name
+//                 text 
+//                 timestamp
+//                 uid
+//             }
+//         }
+//     },
+//     usersDucks: {
+//         [uid]: {
+//             duckIds: [duckID, duckID, duckID]
+//         }
+//     },
+//     likeCount: {
+//         [duckId]: 0 
+//     },
+//     usersLikes: {
+//         [duckId]: true
+//     },
+//     replies: {
+//         [duckId]: {
+//             replies: {
+//                 [replyId]: {
+//                     name
+//                     comment
+//                     uid
+//                     timestamp
+//                     avatar
+//                 }
+//             }
+//         }
+//     }
+// }
+
+Above but with UI and error logic:
+
+{
+    users: {
+        isAuthed,
+        isFetching,
+        error,
+        authedId,
+        [uid]: {
+            lastUpdated,
+            info: {
+                name,
+                uid,
+                avatar,
+            }
+        }
+    },
+    modal: {
+        duck,
+        isOpen,
+    },
+    ducks: {
+        [duckID]: {
+            lastUpdated,
+            info: {
+                avatar
+                duckID
+                name
+                text 
+                timestamp
+                uid
+            }
+        }
+    },
+    usersDucks: {
+        isFetching,
+        error,
+        [uid]: {
+            lastUpdated,
+            duckIds: [duckID, duckID, duckID]
+        }
+    },
+    likeCount: {
+        [duckId]: 0 
+    },
+    usersLikes: {
+        [duckId]: true
+    },
+    replies: {
+        isFetching,
+        error,
+        [duckId]: {
+            replies: {
+                lastUpdated,
+                [replyId]: {
+                    name
+                    comment
+                    uid
+                    timestamp
+                    avatar
+                }
+            }
+        }
+    },
+    listeners: {
+        [listenersId]: true
+    },
+    feed: {
+        isFetching,
+        error,
+        newDucksAvailable,
+        duckIdsToAdd: [duckId, duckId],
+        duckIds: [duckId, duckId]
+    },
+}
