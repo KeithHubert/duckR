35c57e161e84334e71286e7b6ea6acb6f732e490
Keith Hubert <keith.hubert@gmail.com>
2017-12-21 17:30:13 -0500

    downgraded router to 3.0.5

---
+++ app/containers/index.js
@@ -0,0 +1,12 @@
+
+export { default as MainContainer} from './Main/MainContainer'
+
+
+
+
+
+
+
+
+// Not valid JS, but can use babel to make shortcut work 
+// export MainContainer from './Main/MainContainer';
