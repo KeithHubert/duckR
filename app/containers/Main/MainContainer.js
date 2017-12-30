dcdefe63b0e4a37528741e4c3b0341faeb39961b
Keith Hubert <keith.hubert@gmail.com>
2017-12-30 00:56:29 -0500

    auth complete, removed fetching if user is no longer fetching to render rest of ui

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -3,27 +3,60 @@ import PropTypes from 'prop-types'
 import { Navigation } from 'components'
 import { container, innerContainer } from './styles.css'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-
+import { bindActionCreators } from 'redux'
+import * as userActionCreators from 'redux/modules/users'
+import { formatUserInfo } from 'helpers/utils'
+import { firebaseAuth } from 'config/constants'
 
 class MainContainer extends Component {
+  componentDidMount () {
+    firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        const userData = user.providerData[0]
+        const userInfo = formatUserInfo(userData.displayName, userData.photoURL, user.uid)
+        this.props.authUser(user.uid)
+        this.props.fetchingUserSuccess(user.uid, userInfo, Date.now())
+        if (this.props.location.pathname === '/') {
+          this.context.router.replace('feed')
+        }
+      } else {
+        // allows render to fire UI below
+        this.props.removeFetchingUser()
+      }
+    })
+  }
+
   render () {
-    return (
-      <div className={container}>
+    // while the user is still being authed return nothing, then kick off the app
+    return this.props.isFetching === true
+      ? null
+      : <div className={container}>
         <Navigation isAuthed={this.props.isAuthed} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
       </div>
-    )
   }
 }
+MainContainer.contextTypes = {
+  router: PropTypes.object.isRequired}
 
 MainContainer.propTypes = {
   isAuthed: PropTypes.bool.isRequired,
+  authUser: PropTypes.func.isRequired,
+  fetchingUserSuccess: PropTypes.func.isRequired,
+  removeFetchingUser: PropTypes.func.isRequired,
+  isFetching: PropTypes.func.isRequired,
 }
 
-export default withRouter(connect(
-  (state) => ({ isAuthed: state.isAuthed })
-)(MainContainer))
+export default connect(
+  (state) => ({isAuthed: state.isAuthed, isFetching: state.isFetching}),
+  (dispatch) => bindActionCreators(userActionCreators, dispatch)
+)(MainContainer)
+
+// updated and works, need to update rest of app though
+// export default withRouter(connect(
+//   (state) => ({ isAuthed: state.isAuthed })
+// )(MainContainer))
+
 // (state) => ({isAuthed: state.isAuthed}) = mapToState
