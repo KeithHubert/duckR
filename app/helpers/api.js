0697605eab283d655cca81935f96a04c0b240b12
Keith Hubert <keith.hubert@gmail.com>
2018-01-02 14:17:29 -0500

    feed, ducks, usersDucks modals created.

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,27 +1,24 @@
 import { ref } from 'config/constants'
 
-
-
-
-
 function saveToDucks (duck) {
-    // Pushes new data to Firebase, creates random duckId
-    const duckId = ref.child('ducks').push().key
-    // Creates new object which has all the properties of duck and adds the duck Id.
-    const duckPromise = ref.child(`ducks/${duckId}`).set({...duck, duckId})
+  // Pushes new data to Firebase, creates random duckId
+  const duckId = ref.child('ducks').push().key
+  // Creates new object which has all the properties of duck and adds the duck Id.
+  const duckPromise = ref.child(`ducks/${duckId}`).set({...duck, duckId})
 
-    return {
-        duckId,
-        duckPromise
-    }
+  return {
+    duckId,
+    duckPromise,
+  }
 }
 
 function saveUsersDucks (duck, duckId) {
-    return ref.child(`usersDucks/${duck.uid}/${duckId}`)
+  return ref.child(`usersDucks/${duck.uid}/${duckId}`)
+    .set({...duck, duckId})
 }
 
 function saveLikeCount (duckId) {
-
+  return ref.child(`likecount/${duckId}`).set(0)
 }
 
 // encaps all our invocations to the above async functions
