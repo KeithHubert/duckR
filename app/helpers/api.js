dd4a02cb38a90534ffa92e359c9538e0379292c0
Keith Hubert <keith.hubert@gmail.com>
2018-01-03 13:32:44 -0500

    added duck actions to api

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -32,15 +32,49 @@ export function saveDuck (duck) {
   ]).then(() => ({...duck, duckId}))
 }
 
-// listen to 'ducks' endpoint, on the event of a change run function with snapshot
-export function listenToFeed (cb, errorCB) {
+export function listenToFeed (cb, errorCb) {
+  let timesCalled = 0
   ref.child('ducks').on('value', (snapshot) => {
     const feed = snapshot.val() || {}
-    // sort entire feed, give back IDs sorted by timestamp
     const sortedIds = Object.keys(feed).sort((a, b) => {
-      return feed[b].timesptamp = feed[a].timestamp
+      return feed[b].timestamp - feed[a].timestamp
     })
-    // return feed
-    cb({feed, sortedIds})
-  }, errorCB)
+    
+    let initialFetch = timesCalled++ <= 0
+    cb({feed, sortedIds}, initialFetch)
+  }, errorCb)
+}
+
+// // listen to 'ducks' endpoint, on the event of a change run function with snapshot
+// export function listenToFeed (cb, errorCB) {
+//   ref.child('ducks').on('value', (snapshot) => {
+//     const feed = snapshot.val() || {}
+//     // sort entire feed, give back IDs sorted by timestamp
+//     const sortedIds = Object.keys(feed).sort((a, b) => feed[b].timestamp - feed[a].timestamp)
+//     cb({feed, sortedIds})
+//     // return feed
+//   }, errorCB)
+// }
+
+export function fetchUsersLikes (uid) {
+  return ref.child(`usersLikes/${uid}`).once('value')
+    .then((snapshot) => snapshot.val() || {})
+}
+
+export function saveToUsersLikes (uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(true)
+}
+
+export function deleteFromUsersLikes (uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(null)
+}
+
+export function incrementNumberOfLikes (duckId) {
+  return ref.child(`likeCount/${duckId}`)
+    .transaction((currentValue = 0) => currentValue + 1)
+}
+
+export function decrementNumberOfLikes (duckId) {
+  return ref.child(`likeCount/${duckId}`)
+    .transaction((currentValue = 0) => currentValue - 1)
 }
